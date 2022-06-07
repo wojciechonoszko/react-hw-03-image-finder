ef64a9a430f64bf9fc1873dfc0f1f338a1b67066
Wojciech <wojciechonoszko@gmail.com>
2022-06-07 13:01:30 +0200

    loader

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,13 @@
+import Loader from 'react-loader-spinner';
+export const MyLoader = () => {
+    return (
+    <Loader
+      type="Plane"
+      color="#00BFFF"
+      height={80}
+      width={80}
+      timeout={3000}
+      style={{zIndex: '100', position: 'fixed', top: '50%', left: '50%'}}
+    />
+    );
+};
