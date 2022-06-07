a9a2db79c2961c6b4cf7b80f89009700be30fa0c
Wojciech <wojciechonoszko@gmail.com>
2022-06-07 12:50:56 +0200

    gallery item

---
+++ src/components/imageGalleryItem/ImageGalleryItem.js
@@ -0,0 +1,24 @@
+import {ImageGalleryItemContainer} from './ImageGalleryItemStyled';
+import PropTypes from 'prop-types';
+
+const ImageGalleryItem = ({id, imgURL, largeImageURL}) => {
+    return (
+        <ImageGalleryItemContainer>
+            <img
+                src={imgURL}
+                data-large-image-url={largeImageURL}
+                key={id}
+                alt=""
+                className="ImageGalleryItem-image"
+            />
+        </ImageGalleryItemContainer>
+    );
+};
+
+ImageGalleryItem.propTypes = {
+    imgUrl: PropTypes.string.isRequired,
+    largeImageURL: PropTypes.string.isRequired
+
+};
+
+export default ImageGalleryItem;
