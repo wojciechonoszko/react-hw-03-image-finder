84f9b9a73639772c73e9d7a5658bb87bbbaf9478
Wojciech <wojciechonoszko@gmail.com>
2022-06-07 20:24:15 +0200

    api

---
+++ src/Api.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const numPerPage = 12;
+const apiKey = '26533156-9695e0ec144eb89afbc08934a';
+const apiUrl = `https://pixabay.com/api/?key=${apiKey}&
+                image_type=photo&orientation=horizontal&per_page=${numPerPage}`;
+
+export const getImages = async (searchTerm, page) => {
+    try {
+        const response = await axios.get(apiUrl + `&q=${searchTerm}&page=${page}`);
+
+        const result = response.data.hits.map(hit => ({
+            id: hit.id,
+            webformatURL: hit.webformatURL,
+            largeImageURL: hit.largeImageURL
+        }));
+        return result;
+    }   catch (error) {
+        return [];
+    }
+};
+
+// added delay for visible spinner
+
+export const getImagesWithDelay = async (searchTerm, page, delay) => {
+    return getImages(searchTerm, page).then(sleeper(delay));
+};
+
+const sleeper = ms => {
+    return function (x) {
+        return new Promise(resolve => setTimeout(() => resolve(x), ms));
+
+    };
+};
