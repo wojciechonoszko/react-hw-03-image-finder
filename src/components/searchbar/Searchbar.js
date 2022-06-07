c0dd5c3652128a0c62d5853928ca8a9bab4afc41
Wojciech <wojciechonoszko@gmail.com>
2022-06-07 12:19:53 +0200

    bar styles

---
+++ src/components/searchbar/Searchbar.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'ract';
+import {SearchbarHeader, SearchForm} from './SearchbarStyles';
+import PropTypes from 'prop-types';
+
+class Searchbar extends Component {
+    state = {
+        searchTerm: ''
+    }
+
+
+    onInputChange = event => {
+        this.setState({searchTerm: event.target.value});
+    };
+
+    onSumbmit = event => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.searchTerm);
+    };
+
+    render() {
+        return (
+            <SearchbarHeader>
+                <SearchForm onSubmit={this.onSubmit}>
+                    <button type='submit' className="SearchForm-button">
+                        <span className="SearchForm-button-label">Search</span>
+                    </button>
+                    <input
+                    className="SearchForm-input"
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search images and photos"
+                    onChange={this.onInputChange}
+                    />
+                </SearchForm>
+            </SearchbarHeader>
+        );
+    }
+};
+
+Searchbar.propTypes = {
+    onSubmit: PropTypes.func.isRequired
+};
+
+export default Searchbar;
--- src/components/searchbar/SearchbarStyles.js
+++ src/components/searchbar/SearchbarStyles.js
@@ -1,45 +1,71 @@
-import React, {Component} from 'ract';
-import {SearchbarHeader, SearchForm} from './SearchbarStyles';
-import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-class Searchbar extends Component {
-    state = {
-        searchTerm: ''
-    }
-
-
-    onInputChange = event => {
-        this.setState({searchTerm: event.target.value});
-    };
-
-    onSumbmit = event => {
-        event.preventDefault();
-        this.props.onSubmit(this.state.searchTerm);
-    };
-
-    render() {
-        return (
-            <SearchbarHeader>
-                <SearchForm onSubmit={this.onSubmit}>
-                    <button type='submit' className="SearchForm-button">
-                        <span className="SearchForm-button-label">Search</span>
-                    </button>
-                    <input
-                    className="SearchForm-input"
-                    type="text"
-                    autoComplete="off"
-                    autoFocus
-                    placeholder="Search images and photos"
-                    onChange={this.onInputChange}
-                    />
-                </SearchForm>
-            </SearchbarHeader>
-        );
-    }
-};
-
-Searchbar.propTypes = {
-    onSubmit: PropTypes.func.isRequired
-};
-
-export default Searchbar;
+export const SearchbarHeader = styled.header`
+  top: 0;
+  left: 0;
+  position: sticky;
+  z-index: 1100;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 64px;
+  padding-right: 24px;
+  padding-left: 24px;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  color: #fff;
+  background-color: #3f51b5;
+  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+`;
+export const SearchForm = styled.form`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 600px;
+  background-color: #fff;
+  border-radius: 3px;
+  overflow: hidden;
+  .SearchForm-button {
+    display: inline-block;
+    width: 48px;
+    height: 48px;
+    border: 0;
+    background-image: url('https://image.flaticon.com/icons/svg/149/149852.svg');
+    background-size: 40%;
+    background-repeat: no-repeat;
+    background-position: center;
+    opacity: 0.6;
+    transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
+    cursor: pointer;
+    outline: none;
+  }
+  .SearchForm-button:hover {
+    opacity: 1;
+  }
+  .SearchForm-button-label {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    clip-path: inset(50%);
+    border: 0;
+  }
+  .SearchForm-input {
+    display: inline-block;
+    width: 100%;
+    font: inherit;
+    font-size: 20px;
+    border: none;
+    outline: none;
+    padding-left: 4px;
+    padding-right: 4px;
+  }
+  .SearchForm-input::placeholder {
+    font: inherit;
+    font-size: 18px;
+  }
+`;
