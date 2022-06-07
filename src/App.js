51cf8b0f2372a30d80a2b60b3ed26c36c2fca1ee
Wojciech <wojciechonoszko@gmail.com>
2022-06-07 20:42:16 +0200

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import {MyLoader} from '.components/loader/Loader';
+import {getImagesWithDelay} from './Api';
+import Button from './components/button/Button';
+import ImageGallery from './components/imageGallery/ImageGallery';
+import Modal from './components/modal/Modal';
+import Searchbar from './components/searchbar/Searchbar';
+
+const apiDelay = 200;
+
+class App extends Component {
+    state = {
+        pageNum: 1,
+        searchTerm: '',
+        images: [],
+        modalLargeURL: '',
+        showLoader: false
+    };
+
+    componentDidUpdate = () => {
+        window.scrollTo({
+            top: document.documentElement.scroolHeight,
+            behavior: 'smooth'
+        });
+    };
+
+    onSearchSubmit = searchTerm => {
+        this.showLoader();
+
+        getImagesWithDelay(searchTerm, 1, apiDelay).then(images =>
+            this.setState(
+                prevState => ({
+                    images: images,
+                    pageNum: 1,
+                    searchTerm: searchTerm
+                }),
+                this.hideLoader
+            )
+            );
+    };
+
+    onBtnClick = () => {
+        this.showLoader();
+        getImagesWithDelay(
+            this.state.searchTerm,
+            this.state.pageNum + 1,
+            apiDelay
+        ).then(newImages =>
+            this.setState(
+                prevState => ({
+                    images: [...prevState.images, ...newImages],
+                    pageNum: prevState.pageNum + 1
+                }),
+                this.hideLoader
+            )
+            );
+    };
+
+    onItemClick = largeImageUrl => {
+        this.setState({modalLargeURL: largeImageUrl});
+    };
+
+    closeModal = () => {
+        this.setState(prevState =>
+            prevState.showLoader ? null : {showLoader: true}
+        );
+    };
+
+    hideLoader = () => {
+        this.setState(prevState =>
+            prevState.showLoader ? {showLoader: false} : null
+        );
+    };
+
+    
+}
